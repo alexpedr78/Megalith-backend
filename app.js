3bd7de281027adeb517b83b9430bed8b92abbf9d
pedro <alexismoreauavila@gmail.com>
2024-10-13 23:00:12 +0200

    fixing URI

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use("/api/megalith", megalithRoutes);
+app.use("/megalith", megalithRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from the megalith!");
