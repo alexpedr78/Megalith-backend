6d26d486abde1954aade87d7f9edcf80259919ce
pedro <alexismoreauavila@gmail.com>
2024-10-29 18:46:09 +0100

    fixing cors

--- controllers/megalithController.js
+++ controllers/megalithController.js
@@ -75,34 +75,33 @@ const getMegaliths = async (req, res) => {
     res.status(500).json({ message: "Error fetching data", error });
   }
 };
-// get megalith for the map
 const getMegalithsForMap = async (req, res) => {
   try {
     const { name, state, type } = req.query;
 
     const query = {};
-    // If a name is provided, search by name (using case-insensitive regex)
     if (name) {
-      query.name = { $regex: name, $options: "i" }; // Case-insensitive search
+      query.name = { $regex: name, $options: "i" };
     }
-    // Filter by state if provided
     if (state && state !== "-1") {
       query.state = state;
     }
-    // Filter by type if provided
     if (type && type !== "-1") {
       query.type = type;
     }
-    // Execute the query with pagination
+
     const data = await Megalith.find(query);
-    res.status(200).json({
-      data,
-    });
+
+    res.status(200).json({ data });
   } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).json({ message: "Error fetching data", error });
+    console.error("Error in getMegalithsForMap function:", error);
+    res.status(500).json({
+      message: "Error fetching data in getMegalithsForMap",
+      error: error.message,
+    });
   }
 };
+
 // obtain one megalith with his id
 const getMegalithById = async (req, res) => {
   try {
